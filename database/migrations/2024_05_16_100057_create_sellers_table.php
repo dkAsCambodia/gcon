24ee8a08ffd80222904eec8444a3188a627ed551
dkAsCambodia <nitindamo50@gmail.com>
2024-05-16 18:07:27 +0700

    create seller atble

---
+++ database/migrations/2024_05_16_100057_create_sellers_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->id();
+            $table->string('shopName')->nullable();
+            $table->string('businessType')->nullable();
+            $table->string('cuisine')->nullable();
+            $table->string('firstName')->nullable();
+            $table->string('lastName')->nullable();
+            $table->string('phoneNumber')->nullable();
+            $table->string('email')->nullable();
+            $table->string('password')->nullable();
+            $table->string('shopImage')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->string('additionalAddress')->nullable();
+            $table->string('contractStatus')->default('pending');
+            $table->string('status')->default(true);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sellers');
+    }
+};
