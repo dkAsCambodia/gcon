1c6ebf966a1b303c5b3402897d1478ed4235e3da
dkAsCambodia <nitindamo50@gmail.com>
2024-05-29 18:28:26 +0700

    show restaurant on the website inprogress

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
 class Restaurant extends Model
 {
     use HasFactory;
