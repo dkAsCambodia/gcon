f5c7eaae50d9298ec79c58f58a3527c95d671f41
dkAsCambodia <nitindamo50@gmail.com>
2024-07-17 18:27:52 +0700

    show restaurant foods listing and it's action CRUD that showing in   seller dashboard

---
+++ app/Filament/Seller/Resources/RestaurantFoodResource/Pages/CreateRestaurantFood.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Seller\Resources\RestaurantFoodResource\Pages;
+
+use App\Filament\Seller\Resources\RestaurantFoodResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRestaurantFood extends CreateRecord
+{
+    protected static string $resource = RestaurantFoodResource::class;
+}
---
+++ app/Models/RestaurantFoodTranslation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RestaurantFoodTranslation extends Model
+{
+    use HasFactory;
+    protected $guarded = ['id'];
+
+    public function languages()
+    {
+        return $this->belongsTo(Language::class,  'language_id', 'id');
+    }
+
+    public function RestaurantFoodData()
+    {
+        return $this->belongsTo(RestaurantFood::class, 'restaurant_food_id', 'id');
+    }
+
+}
