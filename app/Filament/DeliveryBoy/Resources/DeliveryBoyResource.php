69f0be16dca88e3034b84666b38a790f1c58d9ef
dkAsCambodia <nitindamo50@gmail.com>
2024-09-30 18:15:41 +0700

    work on update delivery boy profile details in dashboard login panel

--- app/Filament/DeliveryBoy/Resources/DeliveryBoyResource.php
+++ app/Filament/DeliveryBoy/Resources/DeliveryBoyResource.php
@@ -12,107 +12,155 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Models\TblGbooking;
 
 class DeliveryBoyResource extends Resource
 {
     protected static ?string $model = DeliveryBoy::class;
-
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-user';
+    public static function getModelLabel(): string{
+        return __('message.View profile');
+    }
 
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                Forms\Components\TextInput::make('BookingType')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('DeleveryBoyLoginId')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('DeliveryBoyId')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('name')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('mobile')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('address')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('city')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('zip')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('state')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('country')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('landmark')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('location')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('lat')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('long')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('status')
-                    ->required()
-                    ->maxLength(255)
-                    ->default(1),
-                Forms\Components\TextInput::make('available_for_delivery')
-                    ->required()
-                    ->maxLength(255)
-                    ->default(1),
-            ]);
+        ->schema([
+            Forms\Components\Select::make('BookingType')
+                ->label(__('message.GBooking Type'))
+                ->options(TblGbooking::where('status', 1)->pluck('BookingType', 'id')) 
+                ->prefixIcon('heroicon-o-rectangle-stack')
+                ->default('2')
+                ->disabled()
+                ->reactive(),
+            Forms\Components\TextInput::make('DeliveryBoyId')
+                ->label(__('message.DeliveryBoyId'))
+                ->readonly()
+                ->maxLength(255),
+            Forms\Components\TextInput::make('name')
+                ->prefixIcon('heroicon-m-user')
+                ->label(__('message.Name'))
+                ->maxLength(255),
+            Forms\Components\TextInput::make('mobile')
+                ->label(__('message.Phone Number'))
+                ->prefixIcon('heroicon-m-phone')
+                ->maxLength(255),
+            Forms\Components\TextInput::make('address')
+                ->label(__('message.Address'))
+                ->prefixIcon('heroicon-m-map-pin')
+                ->maxLength(255),
+            Forms\Components\TextInput::make('city')
+                ->label(__('message.City'))
+                ->prefixIcon('heroicon-m-map-pin')
+                ->maxLength(255),
+            Forms\Components\TextInput::make('zip')
+                ->label(__('message.zip'))
+                ->prefixIcon('heroicon-m-map-pin')
+                ->maxLength(255),
+            Forms\Components\TextInput::make('state')
+                ->label(__('message.State'))
+                ->prefixIcon('heroicon-m-map-pin')
+                ->maxLength(255),
+            Forms\Components\Select::make('country')
+                ->label(__('message.Country'))
+                ->options([
+                    'KH' => 'Cambodia',
+                    'TH' => 'Thailand',
+                    'VN' => 'Vietnam',
+                    'MY' => 'Malaysia',
+                    'ID' => 'Indonesia',
+                    'US' => 'United States',
+                    'PH' => 'Philippines',
+                    'IN' => 'India',
+                    'CN' => 'China',
+                ])
+                ->default('KH')
+                ->prefixIcon('heroicon-m-flag')
+                ->searchable()
+                ->required(),
+            Forms\Components\TextInput::make('landmark')
+                ->label(__('message.landmark'))
+                ->prefixIcon('heroicon-m-map-pin')
+                ->maxLength(255),
+            Forms\Components\TextInput::make('location')
+                ->label(__('message.Location'))
+                ->prefixIcon('heroicon-m-map-pin')
+                ->maxLength(255),
+            Forms\Components\TextInput::make('lat')
+                ->label(__('message.Lat'))
+                ->maxLength(255),
+            Forms\Components\TextInput::make('long')
+                ->label(__('message.Long'))
+                ->maxLength(255),
+            Forms\Components\Toggle::make('available_for_delivery')
+                ->label(__('message.Available for delivery'))
+                ->default('0')
+                ->onIcon('heroicon-m-bolt')
+                ->offIcon('heroicon-m-user')
+                ->onColor('success'),
+        ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
+            ->modifyQueryUsing(fn ($query) => $query->owner()) 
             ->columns([
-                Tables\Columns\TextColumn::make('BookingType')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('DeleveryBoyLoginId')
+                Tables\Columns\TextColumn::make(ucfirst('gbookingdata.BookingType'))
+                    ->label(__('message.Business Type'))
                     ->searchable(),
                 Tables\Columns\TextColumn::make('DeliveryBoyId')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('name')
+                    ->label(__('message.Name'))
                     ->searchable(),
                 Tables\Columns\TextColumn::make('mobile')
+                    ->label(__('message.Phone Number'))
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('deliverBoyLoginData.email')
+                    ->label(__('message.Email'))
                     ->searchable(),
                 Tables\Columns\TextColumn::make('address')
+                    ->label(__('message.Address'))
                     ->searchable(),
                 Tables\Columns\TextColumn::make('city')
+                    ->label(__('message.City'))
                     ->searchable(),
                 Tables\Columns\TextColumn::make('zip')
+                    ->label(__('message.zip'))
                     ->searchable(),
                 Tables\Columns\TextColumn::make('state')
+                    ->label(__('message.State'))
                     ->searchable(),
                 Tables\Columns\TextColumn::make('country')
+                    ->label(__('message.Country'))
                     ->searchable(),
                 Tables\Columns\TextColumn::make('landmark')
+                    ->label(__('message.landmark'))
                     ->searchable(),
-                Tables\Columns\TextColumn::make('location')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('lat')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('long')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('status')
-                    ->searchable(),
-                Tables\Columns\TextColumn::make('available_for_delivery')
-                    ->searchable(),
+                Tables\Columns\IconColumn::make('status')
+                    ->label(__('message.Status'))
+                    ->boolean(),
+                Tables\Columns\IconColumn::make('available_for_delivery')
+                    ->label(__('message.Available for delivery'))
+                    ->boolean(),
                 Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                    ->label(__('message.Created at'))
+                    ->dateTime('d-M-Y')
+                    ->sortable(),
                 Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
+                    ->label(__('message.Updated at'))
+                    ->dateTime('d-M-Y')
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
+            ->defaultSort('created_at', 'desc')
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\ViewAction::make(),
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make()->label(__('message.View'))->modalHeading(__('message.View')),
+                Tables\Actions\EditAction::make()->label(__('message.Edit'))->modalButton(__('message.Save changes')),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -132,9 +180,9 @@ class DeliveryBoyResource extends Resource
     {
         return [
             'index' => Pages\ListDeliveryBoys::route('/'),
-            'create' => Pages\CreateDeliveryBoy::route('/create'),
-            'view' => Pages\ViewDeliveryBoy::route('/{record}'),
-            'edit' => Pages\EditDeliveryBoy::route('/{record}/edit'),
+            // 'create' => Pages\CreateDeliveryBoy::route('/create'),
+            // 'view' => Pages\ViewDeliveryBoy::route('/{record}'),
+            // 'edit' => Pages\EditDeliveryBoy::route('/{record}/edit'),
         ];
     }
 }
--- app/Filament/DeliveryBoy/Resources/DeliveryBoyResource/Pages/ListDeliveryBoys.php
+++ app/Filament/DeliveryBoy/Resources/DeliveryBoyResource/Pages/ListDeliveryBoys.php
@@ -13,7 +13,7 @@ class ListDeliveryBoys extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
