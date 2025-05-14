302c23c661e73d9afbc597e4fef8e9d6051f74d4
dkAsCambodia <nitindamo50@gmail.com>
2025-05-14 18:22:38 +0700

    Insert form data in database for booking table of restaurant

--- app/Livewire/Website/BookingRestaurantTableSeatlayout.php
+++ app/Livewire/Website/BookingRestaurantTableSeatlayout.php
@@ -1,21 +1,18 @@
 <?php
 namespace App\Livewire\Website;
 use Livewire\Component;
-
-use App\Models\Event;
-use App\Models\SittingTableType;
-use App\Models\Currency;
-use App\Models\SittingLayout;
-use App\Models\EventTransaction;
 use DB;
 use Session;
 use Livewire\Attributes\Rule;
+use App\Models\Currency;
 use App\Models\Restaurant;
 use App\Models\Timeslot;
+use App\Models\BookingrestauranttableTransaction;
+use Illuminate\Support\Str;
 
 class BookingRestaurantTableSeatlayout extends Component
 {
-    public $restaurantData, $timeslotList, $special_request;
+    public $restaurantData, $timeslotList, $currencydata, $special_request;
     public $loading = false;
      #[Rule('required')]
     public $date, $time, $name, $email, $address, $paymentType, $preferredSeats='Other', $flat_fee='100';
@@ -26,8 +23,8 @@ class BookingRestaurantTableSeatlayout extends Component
     {
         $this->date = now()->toDateString();
         $this->restaurantData=Restaurant::where(['id' => base64_decode($restaurant_id), 'status' => '1'])->first();
-        // $SittingTableType = SittingTableType::where('sitting_for', 'events')->orderBy('order','ASC')->get();
-         $this->timeslotList=Timeslot::where(['status' => '1'])->orderBy('orderby', 'asc')->get();
+        $this->timeslotList=Timeslot::where(['status' => '1'])->orderBy('orderby', 'asc')->get();
+       
     }
    
 
@@ -37,20 +34,16 @@ class BookingRestaurantTableSeatlayout extends Component
         // Reset the loading state
         sleep(1);
         $this->loading = false;
-          dd($this);
-        // $user_id='';
-        // if(!empty(Session::get('memberdata'))){
-        //     $userdata=Session::get('memberdata');
-        //     $user_id=$userdata['id'];
-        // }
-
+        $concertFormPage = new ConcertFormPage();
+        $userId = $concertFormPage->getuserId($this->name, $this->phone, $this->email, $this->address);
+        $this->currencydata=Currency::where(['currency_code' => 'THB'])->first();
         date_default_timezone_set('Asia/Phnom_Penh');
         $created_at=date("Y-m-d h:i:s");
-        $userId = $this->getuserId($this->name, $this->phone, $this->email, $this->address);
+        $order_key = $this->generateTrackingKey();
         $data=array(
-            'GBooking_id'               => $this->tableDetails->GBooking_id,
-            'cat_id'                    => $this->cat_id,
-            'tableId'                   => $this->tableDetails->id,
+            'GBooking_id'               => $this->restaurantData->GBookingId,
+            'restaurant_id'             => $this->restaurantData->id,
+            'order_key'                 => $order_key,
             'user_id'                   => $userId,
             'name'                      => $this->name,
             'phone'                     => $this->phone,
@@ -59,16 +52,18 @@ class BookingRestaurantTableSeatlayout extends Component
             'no_of_people'              => $this->no_of_people,
             'preferredSeats'            => $this->preferredSeats,
             'quantity'                  => $this->quantity,
+            'special_request'           => $this->special_request,
             'paymentType'               => $this->paymentType,
-            'concert_booking_date'      => $this->date,
-            'concert_arrival_time'      => $this->time,
-            'total_amount'              => $this->tableDetails->tbl_price*$this->quantity,
-            'currency'                  => $this->tableDetails->currencydata->currency_code,
-            'currency_symbol'           => $this->tableDetails->currencydata->currency_symbol,
+            'date'                      => $this->date,
+            'time'                      => $this->time,
+            'total_amount'              => $this->flat_fee,
+            'currency'                  => $this->currencydata->currency_code,
+            'currency_symbol'           => $this->currencydata->currency_symbol,
             'status'                    => 'pending',
             'created_at' =>  $created_at
         );
-        $newTrnasaction = ConcertTblTransaction::create($data);
+        $newTrnasaction = BookingrestauranttableTransaction::create($data);
+        dd($data);
         // if($this->paymentType=='online'){
         //     Session::put('sess_transaction_recordId', $newTrnasaction->id);
         //     return $this->redirect('/paymentOptions'.'/'.base64_encode($this->tableDetails->tbl_price*$this->quantity).'/'.$this->tableDetails->currencydata->currency_symbol.'/'.base64_encode($this->tableDetails->currencydata->currency_code), navigate: true);
@@ -86,6 +81,17 @@ class BookingRestaurantTableSeatlayout extends Component
             // $this->dispatch('toast', message: $msg, notify:'success' ); 
             return $this->redirect('/payment/privacypolicy'.'/'.base64_encode($this->tableDetails->tbl_price*$this->quantity).'/'.$this->tableDetails->currencydata->currency_symbol.'/'.base64_encode($this->tableDetails->currencydata->currency_code).'/'.base64_encode($newTrnasaction->id), navigate: true);
            
+    }
+
+    function generateTrackingKey()
+    {
+        // Format: YYYYMMDDHHSS
+        $prefix = now()->format('YmdHis');
+
+        // Add random 4 digits to make total 16 digits
+        $randomDigits = str_pad(mt_rand(0, 9999), 4, '0', STR_PAD_LEFT);
+
+        return $prefix . $randomDigits;
     }
 
     public function render()
